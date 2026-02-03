acad5acde031e235499b4b2379ac1f0624dae6ec
Bharath Reddy <bharathreddyv2003@gmail.com>
2026-02-03 18:35:17 +0530

    This is the backend Apllication of the version 1

---
+++ index.js
@@ -0,0 +1,29 @@
+const http = require('http')
+const nodemailer = require('nodemailer')
+const SendmailTransport = require('nodemailer/lib/sendmail-transport')
+const { send } = require('process')
+
+const server = http.createServer((req, res) => {
+  res.end("Students")
+})
+
+const transport = nodemailer.createTransport({
+    service:'gmail',
+    auth:{
+        user:'bharathreddyv2003@gmail.com',
+        pass:'jgka epvt tkgb hqhx'
+    }
+
+})
+
+    transport.sendMail({
+        from:"bharathreddyv2003@gmail.com",
+        to:"singhnilesh852117@gmail.com",
+        text:"this is mail",
+        subject:"Mail to sent"
+
+    })
+
+server.listen(3000, () => {
+  console.log("Server running on port 3000")
+})
