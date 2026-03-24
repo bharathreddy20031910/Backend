bccfcb5015f6d2440522b5a91886c4e37fdea95f
Bharath Reddy <bharathreddyv2003@gmail.com>
2026-03-24 20:31:50 +0530

    Final Test Building for this Application 2

--- test/html.test.js
+++ test/html.test.js
@@ -5,7 +5,7 @@ describe('Public HTML page', () => {
   const indexPath = path.join(__dirname, '../public/index.html');
 
   test('index.html exists', () => {
-    expect(fs.existsSync(indexPath)).toBe(true);
+    expect(fs.existsSync(indexPath)).toBe(false);
   });
 
   test('index.html has expected title and heading', () => {
